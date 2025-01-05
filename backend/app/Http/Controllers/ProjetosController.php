7cbc54d0914bd78c12667780ff38f09c410cfc79
Debora Souza <debora.souzalima22@gmail.com>
2025-01-05 14:34:24 -0300

    Criação da tabela projetos, inclusão do grupo de rotas de projetos, criação da model projetos e criação do controller projetos. No controller projetos feito as funções de criar, consultar, deletar, editar e filtrar.

---
+++ backend/app/Http/Controllers/ProjetosController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Projetos;
+
+class ProjetosController extends Controller
+{
+    // criar projeto
+    public function criar(Request $request)
+    {
+        $projeto = new Projetos();
+        $projeto->nome = $request->nome;
+        $projeto->descricao = $request->descricao;
+        $projeto->dataDeInicio = $request->dataDeInicio;
+        $projeto->dataDeConclusao = $request->dataDeConclusaoe;
+        $projeto->pontos = $request->pontos;
+        $projeto->created_by = auth()->id();
+
+        // setando para que não seja null para não dar erro ao cadastrar
+        if (isset($request->prioridade))
+            $projeto->prioridade = $request->prioridade;
+        if (isset($request->prioridade))
+            $projeto->status = $request->status;
+
+        $projeto->save();
+
+        return response('Projeto criado ' . $projeto, 201);
+    }
+
+
+    // consultar individualmente
+    public function consultar($id)
+    {
+        // where('created_by', auth()->id() - consultando apenas projetos que este usuário logado e autenticado criou. Depos será possível consultar também projetos em que o usuário é integrante
+        $projeto = Projetos::where('id', $id)->where('created_by', auth()->id())->first();
+
+        if ($projeto == null)
+            return response('Erro, projeto não localizado', 404);
+
+        return response('Projeto: ' . $projeto, 200);
+    }
+
+    // lista todos os projetos
+    public function listar()
+    {
+        // where('created_by', auth()->id() - listando apenas todos projetos que este usuário logado e autenticado criou.
+        $projeto = Projetos::where('created_by', auth()->id())->get();
+
+        return response('Projetos: ' . $projeto, 200);
+    }
+
+    // deletar um usuário
+    public function deletar($id)
+    {
+        $projeto = Projetos::where('id', $id)->first();
+
+        if ($projeto == null)
+            return response('projeto não deletado', 404);
+
+        $projeto->deleted_by = auth()->id();
+        $projeto->save();
+        $projeto->delete();
+
+        return response('Projeto deletado!', 200);
+    }
+
+    // editar um dado do contato
+    public function editarUmaInforacao(Request $request, $id)
+    {
+        $projeto = Projetos::where('id', $id)->where('created_by', auth()->id())->first();
+
+        if (isset($request->nome))
+            $projeto->nome = $request->nome;
+        if (isset($request->descricao))
+            $projeto->descricao = $request->descricao;
+        if (isset($request->dataDeInicio))
+            $projeto->dataDeInicio = $request->dataDeInicio;
+        if (isset($request->nome))
+            $projeto->dataDeConclusao = $request->dataDeConclusao;
+        if (isset($request->dataDeConclusao))
+            $projeto->pontos = $request->pontos;
+        if (isset($request->pontos))
+            $projeto->nome = $request->nome;
+
+        $projeto->updated_by = auth()->id();
+        $projeto->save();
+
+        return response('Projeto editado ' . $projeto, 200);
+    }
+
+    // filtrar dados dos usuários
+    public function filtrar(Request $request)
+    {
+        // $requestVazio = true; se caso não for informado nada, retorne erro
+        $requestVazio = true;
+
+        $projeto = Projetos::where('created_by', auth()->id());
+
+        if (isset($request->nome)) {
+            $requestVazio = false;
+            $projeto->where('nome', 'like', "%$request->nome%");
+        }
+
+        if (isset($request->prioridade)) {
+            $requestVazio = false;
+            $projeto->where('prioridade', $request->prioridade);
+        }
+
+        if (isset($request->dataDeInicio)) {
+            $requestVazio = false;
+            $projeto->where('dataDeInicio', '>=', $request->dataDeInicio);
+        }
+
+        if (isset($request->dataDeConclusao)) {
+            $requestVazio = false;
+            $projeto->where('dataDeConclusao', '<=', $request->dataDeConclusao);
+        }
+
+        if (isset($request->status)) {
+            $requestVazio = false;
+            $projeto->where('status', $request->status);
+        }
+
+        if ($requestVazio == true)
+            return response('Erro, nenhum filtro informado', 403);
+
+        $projeto = $projeto->get();
+
+        return response($projeto, 200);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+
 use App\Http\Controllers\UsuariosController;
+use App\Http\Controllers\ProjetosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +23,7 @@ Route::post('login', [UsuariosController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
 
+    // Rotas para o login
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('me', [AuthController::class, 'me'])->middleware('auth:api');
 
@@ -32,6 +35,16 @@ Route::middleware('auth:api')->group(function () {
         Route::put('/editar/{id}', [UsuariosController::class, 'editar']); // cedita um usuario
         Route::patch('/editarUmaInformacao/{id}', [UsuariosController::class, 'editarUmaInforacao']); // cedita um usuario
         Route::post('/filtrar', [UsuariosController::class, 'filtrar']); // filtra todos
+    });
+
+    // Rotas para os projetos
+    Route::prefix('projetos')->group(function () {
+        Route::post('/criar', [ProjetosController::class, 'criar']);
+        Route::get('/consultar/{id}', [ProjetosController::class, 'consultar']); // consultar individualmente
+        Route::get('/listar', [ProjetosController::class, 'listar']); // listar todos
+        Route::delete('/deletar/{id}', [ProjetosController::class, 'deletar']); // deleta um usuario
+        Route::patch('/editarUmaInformacao/{id}', [ProjetosController::class, 'editarUmaInforacao']); // cedita um usuario
+        Route::post('/filtrar', [ProjetosController::class, 'filtrar']); // filtra todos
     });
 });
 
