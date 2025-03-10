18342a768f55afc38c54d9f08996a98ea82e38bd
Debora Souza <debora.souzalima22@gmail.com>
2025-03-10 09:07:50 -0300

    Incluindo na função savarArquivo o nome criptografado, caminho e salvando o arquivo no BD.

--- backend/app/Http/Controllers/ArquivosController.php
+++ backend/app/Http/Controllers/ArquivosController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Arquivos;
-
+use DateTime;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ArquivosController extends Controller
 {
@@ -12,10 +13,36 @@ class ArquivosController extends Controller
     public function salvarArquivo(Request $request)
     {
         if ($request->hasFile('arquivo')) {
-            $arquivo = $request->file('arquivo');
-            $nomeDoArquivo = $arquivo->getClientOriginalName();
+            $timeStampAtual = new DateTime();
+            $timeStampAtual = $timeStampAtual->getTimestamp();
 
-            $salvar = $arquivo->storeAs('uploads', $nomeDoArquivo, 'local');
+            $arquivo = $request->file('arquivo');
+            $nomeDoArquivoOriginal = $arquivo->getClientOriginalName();
+            $nomeDoArquivoCriptografado = $timeStampAtual . '_' . Str::random(32);
+
+            $salvar = $arquivo->storeAs('uploads', $nomeDoArquivoOriginal, 'local');
+            $caminho = 'uploads/';
+
+            $arquivoBd = new Arquivos();
+            $arquivoBd->nome_original =  $nomeDoArquivoOriginal;
+            $arquivoBd->nome_criptografado =  $nomeDoArquivoCriptografado;
+            $arquivoBd->caminho =  $caminho;
+            $arquivoBd->created_by = auth()->id();
+            $arquivoBd->save();
+
+            return response('Arquivo salvo: ' . $arquivoBd, 201);
+        } else {
+            return response('Erro: ' . 422);
         }
+    }
+
+    // função para download dos arquivos
+    public function download($id)
+    {
+        $arquivo = Arquivos::where('id', $id)->first();
+        $caminho = storage_path('app/') . $arquivo->caminho . $arquivo->nome_criptografado;
+        $nomeOriginal = $arquivo->nome_original;
+
+        return response()->download($caminho, $nomeOriginal);
     }
 }
