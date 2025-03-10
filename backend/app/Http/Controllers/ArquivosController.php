66df026bcb3c9a31b09d32f455cfee2049414cfa
Debora Souza <debora.souzalima22@gmail.com>
2025-03-09 21:28:59 -0300

    Criando a função de salvar arquivos, salvando na pasta uploads.

--- backend/app/Http/Controllers/ArquivosController.php
+++ backend/app/Http/Controllers/ArquivosController.php
@@ -8,5 +8,14 @@ use Illuminate\Http\Request;
 
 class ArquivosController extends Controller
 {
-    //
+    // função salvar arquivos
+    public function salvarArquivo(Request $request)
+    {
+        if ($request->hasFile('arquivo')) {
+            $arquivo = $request->file('arquivo');
+            $nomeDoArquivo = $arquivo->getClientOriginalName();
+
+            $salvar = $arquivo->storeAs('uploads', $nomeDoArquivo, 'local');
+        }
+    }
 }
